7e0078ade6c89e27afb8bcaeca1732a77a77b88f
t.koike <t.koike929@gmail.com>
2024-07-09 22:56:31 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/core/options.lua

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -75,32 +75,7 @@ opt.scrolloff = 10
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 opt.hlsearch = true
 
--- TODO これはおそらく上記の設定と被ってるので見直す
 -- 24ビットRGBカラー有効化
-vim.api.nvim_set_option('termguicolors', true)
--- ファイル末尾に移動した際に4行分の余白設定
-vim.api.nvim_set_option('scrolloff', 4)
--- 検索時に大文字小文字無視
-vim.api.nvim_set_option('ignorecase', true)
--- 検索時に大文字が含まれていたらignorecaseを無効化
-vim.api.nvim_set_option('smartcase', true)
--- 置換時に画面下部に検索結果を表示
-vim.api.nvim_set_option('inccommand', 'split')
--- クリップボードの有効化
-vim.api.nvim_set_option('clipboard', 'unnamedplus')
-
--- window
--- 行番号表示
-vim.api.nvim_win_set_option(0, 'number', true)
--- 相対行番号表示
--- vim.api.nvim_win_set_option(0, 'relativenumber', true)
--- カーソル行を強調
-vim.api.nvim_win_set_option(0, 'cursorline', true)
--- 標識のためのスペースを最左列に設ける
-vim.api.nvim_win_set_option(0, 'signcolumn', 'yes:1')
+opt.termguicolors = true
 -- テキストの折り返しを無効化
-vim.api.nvim_win_set_option(0, 'wrap', false)
--- 非表示文字の可視化
-vim.api.nvim_win_set_option(0, 'list', true)
--- 指定したカラム列を強調
--- vim.api.nvim_win_set_option(0, 'colorcolumn', '100')
+opt.wrap = false
