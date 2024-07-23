66835dc3668514b567b3fb5876924c2db54307a0
koike <t.koike929@gmail.com>
2024-07-23 23:08:23 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/core/lazy.lua

--- dot_config/nvim/lua/core/lazy.lua
+++ dot_config/nvim/lua/core/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({ { import = 'plugins' }, { import = 'plugins.lsp' }, { import = 'plugins.colorscheme' } }, {
+require('lazy').setup({ { import = 'plugins' }, { import = 'plugins.lsp' } }, {
   checker = {
     enabled = true,
     notify = true,
