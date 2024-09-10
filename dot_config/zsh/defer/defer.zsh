983f01701b79c9fe363ca2c59c5089939ddaeb8d
koike <t.koike929@gmail.com>
2024-09-10 19:52:24 +0900

    feat: add fzf cd functions

--- dot_config/zsh/defer/defer.zsh
+++ dot_config/zsh/defer/defer.zsh
@@ -41,6 +41,162 @@ bindkey '^o' fzf_cd
 export FZF_DEFAULT_OPTS='--reverse --border --ansi --bind="ctrl-d:print-query,ctrl-p:replace-query" --inline-info --height 80%'
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 
+
+# -----------------------------------------------------------------------------
+# directory
+# -----------------------------------------------------------------------------
+
+# fzf --preview command for file and directory
+if type bat >/dev/null 2>&1; then
+    FZF_PREVIEW_CMD='bat --color=always --plain --line-range :$FZF_PREVIEW_LINES {}'
+elif type pygmentize >/dev/null 2>&1; then
+    FZF_PREVIEW_CMD='head -n $FZF_PREVIEW_LINES {} | pygmentize -g'
+else
+    FZF_PREVIEW_CMD='head -n $FZF_PREVIEW_LINES {}'
+fi
+
+# zdd - cd to selected directory
+zdd() {
+  local dir
+  dir="$(
+    find "${1:-.}" -path '*/\.*' -prune -o -type d -print 2> /dev/null \
+      | fzf +m \
+          --preview='tree -C {} | head -n $FZF_PREVIEW_LINES' \
+          --preview-window='right:hidden:wrap' \
+          --bind=ctrl-v:toggle-preview \
+          --bind=ctrl-x:toggle-sort \
+          --header='(view:ctrl-v) (sort:ctrl-x)' \
+  )" || return
+  cd "$dir" || return
+}
+
+# zda - including hidden directories
+zda() {
+  local dir
+  dir="$(
+    find "${1:-.}" -type d 2> /dev/null \
+      | fzf +m \
+          --preview='tree -C {} | head -n $FZF_PREVIEW_LINES' \
+          --preview-window='right:hidden:wrap' \
+          --bind=ctrl-v:toggle-preview \
+          --bind=ctrl-x:toggle-sort \
+          --header='(view:ctrl-v) (sort:ctrl-x)' \
+  )" || return
+  cd "$dir" || return
+}
+
+# zdr - cd to selected parent directory
+zdr() {
+  local dirs=()
+  local parent_dir
+
+  get_parent_dirs() {
+    if [[ -d "$1" ]]; then dirs+=("$1"); else return; fi
+    if [[ "$1" == '/' ]]; then
+      for _dir in "${dirs[@]}"; do echo "$_dir"; done
+    else
+      get_parent_dirs "$(dirname "$1")"
+    fi
+  }
+
+  parent_dir="$(
+    get_parent_dirs "$(realpath "${1:-$PWD}")" \
+      | fzf +m \
+          --preview 'tree -C {} | head -n $FZF_PREVIEW_LINES' \
+          --preview-window='right:hidden:wrap' \
+          --bind=ctrl-v:toggle-preview \
+          --bind=ctrl-x:toggle-sort \
+          --header='(view:ctrl-v) (sort:ctrl-x)' \
+  )" || return
+
+  cd "$parent_dir" || return
+}
+
+# zst - cd into the directory from stack
+zst() {
+  local dir
+  dir="$(
+    dirs \
+      | sed 's#\s#\n#g' \
+      | uniq \
+      | sed "s#^~#$HOME#" \
+      | fzf +s +m -1 -q "$*" \
+            --preview='tree -C {} | head -n $FZF_PREVIEW_LINES' \
+            --preview-window='right:hidden:wrap' \
+            --bind=ctrl-v:toggle-preview \
+            --bind=ctrl-x:toggle-sort \
+            --header='(view:ctrl-v) (sort:ctrl-x)' \
+  )"
+  # check $dir exists for Ctrl-C interrupt
+  # or change directory to $HOME (= no value cd)
+  if [[ -d "$dir" ]]; then
+    cd "$dir" || return
+  fi
+}
+
+# zdf - cd into the directory of the selected file
+zdf() {
+  local file
+  file="$(fzf +m -q "$*" \
+           --preview="${FZF_PREVIEW_CMD}" \
+           --preview-window='right:hidden:wrap' \
+           --bind=ctrl-v:toggle-preview \
+           --bind=ctrl-x:toggle-sort \
+           --header='(view:ctrl-v) (sort:ctrl-x)' \
+       )"
+  cd "$(dirname "$file")" || return
+}
+
+
+# zd - cd into selected directory with options
+# The super function of zdd, zda, zdr, zst, zdf, zz
+zd() {
+  read -r -d '' helptext <<EOF
+usage: zd [OPTIONS]
+  zd: cd to selected options below
+OPTIONS:
+  -d [path]: Directory (default)
+  -a [path]: Directory included hidden
+  -r [path]: Parent directory
+  -s [query]: Directory from stack
+  -f [query]: Directory of the selected file
+  -h: Print this usage
+EOF
+
+  usage() {
+    echo "$helptext"
+  }
+
+  if [[ -z "$1" ]]; then
+    # no arg
+    zdd
+  elif [[ "$1" == '..' ]]; then
+    # arg is '..'
+    shift
+    zdr "$1"
+  elif [[ "$1" == '-' ]]; then
+    # arg is '-'
+    shift
+    zst "$*"
+  elif [[ "${1:0:1}" != '-' ]]; then
+    # first string is not -
+    zdd "$(realpath "$1")"
+  else
+    # args is start from '-'
+    while getopts darfszh OPT; do
+      case "$OPT" in
+        d) shift; zdd  "$1";;
+        a) shift; zda "$1";;
+        r) shift; zdr "$1";;
+        s) shift; zst "$*";;
+        f) shift; zdf "$*";;
+        h) usage; return 0;;
+        *) usage; return 1;;
+      esac
+    done
+  fi
+}
+
 # 1password
 ## enable plugins
 source $XDG_CONFIG_HOME/op/plugins.sh
