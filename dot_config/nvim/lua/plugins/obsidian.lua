1ef32d296f4406c4474b58b485f6f45340dc9c48
tk09291197 <t.koike929@gmail.com>
2024-09-27 15:35:19 +0900

    chore: devcontainer環境下ではobsidian.nvimはloadしない

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -2,6 +2,9 @@ return {
   'epwalsh/obsidian.nvim',
   version = '*', -- recommended, use latest release instead of latest commit
   lazy = true,
+  cond = function()
+    return os.getenv 'REMOTE_CONTAINERS' == nil
+  end,
   ft = 'markdown',
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
