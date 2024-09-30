a808e33dd7cace82917beb24939c1d6b6f65cf10
tk09291197 <t.koike929@gmail.com>
2024-09-30 17:38:21 +0900

    feat(nvim): update lspsaga settings

--- dot_config/nvim/lua/plugins/lspsaga.lua
+++ dot_config/nvim/lua/plugins/lspsaga.lua
@@ -1,8 +1,28 @@
+-- lspsaga
+-- https://nvimdev.github.io/lspsaga/
 return {
 	"nvimdev/lspsaga.nvim",
 	event = "LspAttach",
+
 	config = function()
-		require("lspsaga").setup({})
+		vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc")
+		-- see default config https://github.com/nvimdev/lspsaga.nvim/blob/main/lua/lspsaga/init.lua
+		require("lspsaga").setup({
+			implement = {
+				enable = false,
+			},
+			hover = {
+				-- linuxで開かない場合はxdg-settingsでxdg-openが設定されているか確認すること。
+				-- https://wiki.archlinux.org/title/Xdg-utils#xdg-settings
+				open_cmd = "!floorp",
+			},
+			symbol_in_winbar = {
+				enable = false,
+			},
+			lightbulb = {
+				enable = false,
+			},
+		})
 	end,
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter", -- optional
