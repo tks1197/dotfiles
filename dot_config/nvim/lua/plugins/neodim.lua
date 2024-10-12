9bc84b047993ca7d0250dc500f54bca0d3bf75cf
tk09291197 <t.koike929@gmail.com>
2024-10-12 23:48:45 +0900

    style(lua): format file

--- dot_config/nvim/lua/plugins/neodim.lua
+++ dot_config/nvim/lua/plugins/neodim.lua
@@ -1,25 +1,26 @@
 -- 未使用の変数や関数の色合いを変更することのできるプラグイン
 -- https://github.com/zbirenbaum/neodim
 return {
-  'zbirenbaum/neodim',
-  event = 'LspAttach',
-  config = function()
-    require('neodim').setup {
-      refresh_delay = 75,
-      alpha = 0.5,
-      blend_color = '#000000',
-      hide = {
-        underline = true,
-        virtual_text = true,
-        signs = true,
-      },
-      regex = {
-        '[uU]nused',
-        '[nN]ever [rR]ead',
-        '[nN]ot [rR]ead',
-      },
-      priority = 128,
-      disable = {},
-    }
-  end,
+	"zbirenbaum/neodim",
+	enabled = false,
+	event = "LspAttach",
+	config = function()
+		require("neodim").setup({
+			refresh_delay = 75,
+			alpha = 0.5,
+			blend_color = "#000000",
+			hide = {
+				underline = true,
+				virtual_text = true,
+				signs = true,
+			},
+			regex = {
+				"[uU]nused",
+				"[nN]ever [rR]ead",
+				"[nN]ot [rR]ead",
+			},
+			priority = 128,
+			disable = {},
+		})
+	end,
 }
