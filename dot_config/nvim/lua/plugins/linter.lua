cd8062e485dcd20da2d180b46a2405c2cbbb91d0
tk09291197 <t.koike929@gmail.com>
2024-10-14 17:11:36 +0900

    feat(yaml,neovim): add yamllint settings

--- dot_config/nvim/lua/plugins/linter.lua
+++ dot_config/nvim/lua/plugins/linter.lua
@@ -5,10 +5,10 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = {
-			-- python = { 'ruff' },
 			-- markdown = {
 			-- 'markdownlint',
 			-- },
+			yaml = { "yamllint" },
 		}
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
