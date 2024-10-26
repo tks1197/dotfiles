2757dd1e609e29ec25172fd98ee1e9d941e89901
tk09291197 <t.koike929@gmail.com>
2024-10-27 08:44:34 +0900

    feat(tmux,neovim): udpate tmux settings

---
+++ dot_config/nvim/lua/plugins/vim-tpipeline.lua
@@ -0,0 +1,4 @@
+-- vim-tpipeline
+-- vimのstatuslineをtmuxのstatuslineに埋め込むプラグイン
+-- https://github.com/vimpostor/vim-tpipeline
+return { "vimpostor/vim-tpipeline" }
--- dot_config/zsh/defer/defer.zsh
+++ dot_config/zsh/defer/defer.zsh
@@ -1,6 +1,24 @@
 # zsh keybind
 ## see docs https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html#index-binding-keys
 bindkey -r '^J' # Ctrl-j
+# sesh(tmux session manager) keybind
+# https://github.com/joshmedeski/sesh?tab=readme-ov-file#zsh-keybind
+function sesh-sessions() {
+  {
+    exec </dev/tty
+    exec <&1
+    local session
+    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+    zle reset-prompt > /dev/null 2>&1 || true
+    [[ -z "$session" ]] && return
+    sesh connect $session
+  }
+}
+
+zle     -N             sesh-sessions
+bindkey -M emacs '\es' sesh-sessions
+bindkey -M vicmd '\es' sesh-sessions
+bindkey -M viins '\es' sesh-sessions
 # git wrapper
 function git () {
   local ispush
