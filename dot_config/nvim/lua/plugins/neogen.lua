e83aa467b13ad1af34f3d0c8796bb979edb0a5bb
tk09291197 <t.koike929@gmail.com>
2024-10-26 16:04:27 +0900

    feat(nevim): add negen.nvim

---
+++ dot_config/nvim/lua/plugins/neogen.lua
@@ -0,0 +1,9 @@
+-- neogin.nvim
+-- https://github.com/danymat/neogen
+-- docstring等の注釈を生成するプラグイン
+return {
+	"danymat/neogen",
+	config = true,
+	-- Uncomment next line if you want to follow only stable versions
+	version = "*",
+}
