e16e605af37c0cdeecf33166c549d847f97ac858
tk09291197 <t.koike929@gmail.com>
2024-11-09 20:28:20 +0900

    chore(nvim): delete unused plugins

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -89,6 +89,19 @@
 -- Then, because we use the `config` key, the configuration only runs
 -- after the plugin has been loaded:
 --  config = function() ... end
+--
+-- This is often very useful to both group configuration, as well as handle
+-- lazy loading plugins that don't need to be loaded immediately at startup.
+--
+-- For example, in the following configuration, we use:
+--  event = 'VimEnter'
+--
+-- which loads which-key before all the UI elements are loaded. Events can be
+-- normal autocommands events (`:help autocmd-events`).
+--
+-- Then, because we use the `config` key, the configuration only runs
+-- after the plugin has been loaded:
+--  config = function() ... end
 return -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
 --
 -- This is often very useful to both group configuration, as well as handle
@@ -104,35 +117,30 @@ return -- NOTE: Plugins can also be configured to run Lua code when they are loa
 -- after the plugin has been loaded:
 --  config = function() ... end
 { -- Useful plugin to show you pending keybinds.
-  'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
+	"folke/which-key.nvim",
+	event = "VimEnter", -- Sets the loading event to 'VimEnter'
+	config = function() -- This is the function that runs, AFTER loading
+		require("which-key").setup()
 
-    -- Document existing key chains
-    require('which-key').add {
-      -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      -- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      -- ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      --  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      { '<leader>*', '*``cgn', desc = 'edit astarisk focus words' },
-      {
-        -- Nested mappings are allowed and can be added in any order
-        -- Most attributes can be inherited or overridden on any level
-        -- There's no limit to the depth of nesting
-        mode = { 'n', 'v' }, -- NORMAL and VISUAL mode
-        { '<leader>Q', '<cmd>q<cr>', desc = 'Quit' }, -- no need to specify mode since it's inherited
-        { '<leader>W', '<cmd>w<cr>', desc = 'Write' },
-      },
-      { '<leader>t', desc = '[T]oggle' },
-      { '<leader>tt', desc = '[T]erminal' },
-      { '<leader>ttf', '<cmd>ToggleTerm direction=float<cr>', desc = '[F]loat' },
-      { '<leader>tth', '<cmd>ToggleTerm size=10 direction=horizontal<cr>', desc = '[H]orizontal' },
-      { '<leader>ttv', '<cmd>ToggleTerm size=80 direction=vertical<cr>', desc = '[V]artical' },
-      -- },
-      { '<leader>h', desc = 'Git [H]unk', mode = { 'n', 'v' } },
-    }
-  end,
+		-- Document existing key chains
+		require("which-key").add({
+			-- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+			-- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+			-- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+			-- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+			-- ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+			--  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+			{ "<leader>*", "*``cgn", desc = "edit astarisk focus words" },
+			{
+				-- Nested mappings are allowed and can be added in any order
+				-- Most attributes can be inherited or overridden on any level
+				-- There's no limit to the depth of nesting
+				mode = { "n", "v" }, -- NORMAL and VISUAL mode
+				{ "<leader>Q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+				{ "<leader>W", "<cmd>w<cr>", desc = "Write" },
+			},
+			-- },
+			{ "<leader>h", desc = "Git [H]unk", mode = { "n", "v" } },
+		})
+	end,
 }
