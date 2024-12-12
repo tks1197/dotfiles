608a35771c0916c073768f8c7184646bc4982b67
tk09291197 <t.koike929@gmail.com>
2024-12-12 17:11:34 +0900

    feat(conform.nvim): user commands to quickly enable/disable autoformatting

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,10 @@
-return { -- Autoformat
+-- conform.nvim(autoformat plugin)
+-- https://github.com/stevearc/conform.nvim/
+return {
 	"stevearc/conform.nvim",
-	lazy = false,
+	event = "BufWritePre",
+	cmd = "ConformInfo",
+	lazy = true,
 	keys = {
 		{
 			"<leader>f",
@@ -11,36 +15,48 @@ return { -- Autoformat
 			desc = "[F]ormat buffer",
 		},
 	},
-	opts = {
-		notify_on_error = false,
-		format_on_save = function(bufnr)
-			-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- have a well standardized coding style. You can add additional
-			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
-			return {
-				timeout_ms = 2500,
-				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-			}
-		end,
-		formatters_by_ft = {
-			sh = { "shfmt" },
-			json = { "jq" },
-			lua = { "stylua" },
-			-- Conform can also run multiple formatters sequentially
-			python = { "ruff_fix", "ruff_format" },
-			-- toml = { "taplo" },
-			markdown = { "markdownlint-cli2" },
-			-- rust = { 'rustfmt' },
-			--
-			-- You can use a sub-list to tell conform to run *until* a formatter
-			-- is found.
-			-- javascript = { { "prettierd", "prettier" } },
-		},
-		formatters = {
-			shfmt = {
-				prepend_args = { "-i", "2" },
+	config = function()
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+		require("conform").setup({
+			notify_on_error = false,
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				local disable_filetypes = { c = true, cpp = true }
+				return {
+					timeout_ms = 2500,
+					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+				}
+			end,
+			formatters_by_ft = {
+				sh = { "shfmt" },
+				json = { "jq" },
+				lua = { "stylua" },
+				python = { "ruff_fix", "ruff_format" },
+				markdown = { "markdownlint-cli2" },
 			},
-		},
-	},
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+			},
+		})
+	end,
 }
