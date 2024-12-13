c71bd96ec8efb4c5d3709ea47d1eb7ca2ec8cf4a
tk09291197 <t.koike929@gmail.com>
2024-12-13 20:21:17 +0900

    chore(nvim): migrate nvim-cmp to blink.cmp

---
+++ dot_config/nvim/lua/plugins/lsp/blink-cmp.lua
@@ -0,0 +1,106 @@
+return {
+	"saghen/blink.cmp",
+	lazy = false, -- lazy loading handled internally
+	-- optional: provides snippets for the snippet source
+	dependencies = "rafamadriz/friendly-snippets",
+
+	-- use a release tag to download pre-built binaries
+	version = "v0.*",
+	-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	-- build = 'cargo build --release',
+	-- If you use nix, you can build from source using latest nightly rust with:
+	-- build = 'nix run .#build-plugin',
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		-- 'default' for mappings similar to built-in completion
+		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+		-- see the "default configuration" section below for full documentation on how to define
+		-- your own keymap.
+		keymap = { preset = "default" },
+		-- Experimental signature help support
+		completion = {
+			documentation = {
+				-- Controls whether the documentation window will automatically show when selecting a completion item
+				auto_show = true,
+				-- Delay before showing the documentation window
+				auto_show_delay_ms = 500,
+				-- Delay before updating the documentation window when selecting a new item,
+				-- while an existing item is still visible
+				update_delay_ms = 50,
+				-- Whether to use treesitter highlighting, disable if you run into performance issues
+				treesitter_highlighting = true,
+				window = {
+					min_width = 10,
+					max_width = 60,
+					max_height = 20,
+					border = "padded",
+					winblend = 0,
+					winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+					-- Note that the gutter will be disabled when border ~= 'none'
+					scrollbar = true,
+					-- Which directions to show the documentation window,
+					-- for each of the possible menu window directions,
+					-- falling back to the next direction when there's not enough space
+					direction_priority = {
+						menu_north = { "e", "w", "n", "s" },
+						menu_south = { "e", "w", "s", "n" },
+					},
+				},
+			},
+			-- Displays a preview of the selected item on the current line
+			ghost_text = {
+				enabled = true,
+			},
+		},
+		signature = {
+			enabled = true,
+			trigger = {
+				blocked_trigger_characters = {},
+				blocked_retrigger_characters = {},
+				-- When true, will show the signature help window when the cursor comes after a trigger character when entering insert mode
+				show_on_insert_on_trigger_character = true,
+			},
+			window = {
+				min_width = 1,
+				max_width = 100,
+				max_height = 10,
+				border = "padded",
+				winblend = 0,
+				winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
+				scrollbar = false, -- Note that the gutter will be disabled when border ~= 'none'
+				-- Which directions to show the window,
+				-- falling back to the next direction when there's not enough space,
+				-- or another window is in the way
+				direction_priority = { "n", "s" },
+				-- Disable if you run into performance issues
+				treesitter_highlighting = true,
+			},
+		},
+		appearance = {
+			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- Useful for when your theme doesn't support blink.cmp
+			-- will be removed in a future release
+			use_nvim_cmp_as_default = true,
+			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
+
+		-- default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, via `opts_extend`
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+			-- optionally disable cmdline completions
+			-- cmdline = {},
+		},
+
+		-- experimental signature help support
+		-- signature = { enabled = true }
+	},
+	-- allows extending the providers array elsewhere in your config
+	-- without having to redefine it
+	opts_extend = { "sources.default" },
+}
--- dot_config/nvim/lua/plugins/lsp/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -5,6 +5,7 @@ return { -- LSP Configuration & Plugins
 		{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		{ "saghen/blink.cmp" },
 		-- Useful status updates for LSP.
 		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 		{ "j-hui/fidget.nvim", opts = {
@@ -142,7 +143,7 @@ return { -- LSP Configuration & Plugins
 		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities())
 
 		-- Enable the following language servers
 		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
