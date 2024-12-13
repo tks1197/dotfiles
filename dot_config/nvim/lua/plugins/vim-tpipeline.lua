bc70a04c3f5e6a29efc14a04ca727e6a1293051f
tk09291197 <t.koike929@gmail.com>
2024-12-13 23:35:19 +0900

    fix(nvim,tmux): fix nvim statuline was not embeded in tmux

--- dot_config/nvim/lua/plugins/vim-tpipeline.lua
+++ dot_config/nvim/lua/plugins/vim-tpipeline.lua
@@ -7,12 +7,11 @@ return {
 	"vimpostor/vim-tpipeline",
 	event = "VeryLazy",
 	init = function()
-		-- vim.g.tpipeline_autoembed = 0
-		-- vim.g.tpipeline_statusline = ""
+		vim.g.tpipeline_autoembed = 0
+		vim.g.tpipeline_statusline = ""
 	end,
 	config = function()
 		vim.cmd.hi({ "link", "StatusLine", "WinSeparator" })
-		vim.g.tpipeline_statusline = ""
 		vim.o.laststatus = 0
 		vim.o.fillchars = "stl:─,stlnc:─"
 	end,
