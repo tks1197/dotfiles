2ea9c9ef31362a5a5cc8d5c589aa7ff19ac192db
tk09291197 <t.koike929@gmail.com>
2024-12-14 00:56:09 +0900

    style: format lua files

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,14 +1,12 @@
-# https://neovim.io/doc/user/lua.html#vim.loader
 vim.loader.enable()
 
-require 'config.options'
-require 'config.filetypes'
-require 'config.lazy'
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'VeryLazy',
-  callback = function()
-    require 'config.autocmds'
-    require 'config.keymaps'
-  end,
+require("config.options")
+require("config.filetypes")
+require("config.lazy")
+vim.api.nvim_create_autocmd("User", {
+	pattern = "VeryLazy",
+	callback = function()
+		require("config.autocmds")
+		require("config.keymaps")
+	end,
 })
-
--- dot_config/wezterm/mousebinds.lua
+++ dot_config/wezterm/mousebinds.lua
@@ -1,12 +1,11 @@
-local act = require('wezterm').action
+local act = require("wezterm").action
 
 return {
   mouse_bindings = {
     {
-      event = { Down = { streak = 1, button = 'Left' } },
-      mods = 'NONE',
-      action = act.EmitEvent 'show-title-bar',
-    }
-  }
+      event = { Down = { streak = 1, button = "Left" } },
+      mods = "NONE",
+      action = act.EmitEvent("show-title-bar"),
+    },
+  },
 }
-
