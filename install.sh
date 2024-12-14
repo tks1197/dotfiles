6e1c1e653a721fd6836b1fc83fd8b5944d00cee6
tks1197 <t.koike929@gmail.com>
2024-12-15 01:55:58 +0900

    chore: update username

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin" \
-    init --apply --verbose --no-tty tk09291197 --promptString email=test@example.com,name=user1
+    init --apply --verbose --no-tty tks1197 --promptString email=test@example.com,name=user1
