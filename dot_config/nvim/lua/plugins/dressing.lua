d7596e70eb38e9bdef7bccc70f72455c60023ab3
tks1197 <t.koike929@gmail.com>
2024-12-21 13:52:40 +0900

    chore(nvim): delete unused plugin setting files

--- dot_config/nvim/lua/plugins/dressing.lua
+++
@@ -1,6 +0,0 @@
--- dressing.nvim(Neovim plugin to improve the default vim.ui interfaces)
--- https://github.com/stevearc/dressing.nvim
-return {
-	"stevearc/dressing.nvim",
-	opts = {},
-}
--- dot_config/nvim/lua/plugins/schema-store.lua
+++
@@ -1,7 +0,0 @@
--- schemastore.nvim
--- jsonのスキーマのコレクションであるschemastore(https://github.com/SchemaStore/schemastore)を
--- NeovimのLSPであるyamlls及びjsonlsで簡単に利用するためのプラグイン。
-return {
-	"b0o/schemastore.nvim",
-	enabled = false,
-}
