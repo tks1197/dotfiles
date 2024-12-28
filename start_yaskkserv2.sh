2eb13bbea0500cfde191d5216d8bfec6499e5c9e
tks1197 <t.koike929@gmail.com>
2024-12-28 09:22:43 +0900

    chore(skk): update path

--- start_yaskkserv2.sh
+++ start_yaskkserv2.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 pushd "$HOME/.local/share/skk/" || exit 1
-yaskksrrv2_make_dictionary --dictionary-filename=/tmp/dictionary.yaskkserv2 \
+"$HOME"/.local/bin/yaskkserv2_make_dictionary --dictionary-filename=/tmp/dictionary.yaskkserv2 \
     SKK-JISYO.L SKK-JISYO.geo SKK-JISYO.jinmei SKK-JISYO.propernoun SKK-JISYO.station &&
     yaskkserv2 /tmp/dictionary.yaskkserv2 \
         --google-cache-filename=/tmp/yaskkserv2-google-suggest.cache
