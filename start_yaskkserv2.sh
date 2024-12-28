1d145206c967490f85a5f22b4ccf5b0423b2ed0e
tks1197 <t.koike929@gmail.com>
2024-12-28 17:27:48 +0900

    chore(skk): inclue user dict

--- start_yaskkserv2.sh
+++ start_yaskkserv2.sh
@@ -2,6 +2,6 @@
 
 pushd "$HOME/.local/share/skk/" || exit 1
 "$HOME"/.local/bin/yaskkserv2_make_dictionary --dictionary-filename=/tmp/dictionary.yaskkserv2 \
-    SKK-JISYO.L SKK-JISYO.geo SKK-JISYO.jinmei SKK-JISYO.propernoun SKK-JISYO.station &&
+    SKK-JISYO.L SKK-JISYO.geo SKK-JISYO.jinmei SKK-JISYO.propernoun SKK-JISYO.station SKK-JISYO-USER.euc &&
     yaskkserv2 /tmp/dictionary.yaskkserv2 \
         --google-cache-filename=/tmp/yaskkserv2-google-suggest.cache
