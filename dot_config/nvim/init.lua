373e9bb6b4ac8fd2a549eb934304d076daaee2fd
tks1197 <t.koike929@gmail.com>
2024-12-31 22:18:43 +0900

    refactor(nvim): update keymap setting

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,6 +7,5 @@ vim.api.nvim_create_autocmd("User", {
 	pattern = "VeryLazy",
 	callback = function()
 		require("config.autocmds")
-		require("config.keymaps")
 	end,
 })
--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -1,145 +1,27 @@
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
-return -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
-{ -- Useful plugin to show you pending keybinds.
+-- https://github.com/folke/which-key.nvim
+return {
 	"folke/which-key.nvim",
-	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	config = function() -- This is the function that runs, AFTER loading
+	event = "VimEnter",
+	config = function()
+		local keymap = vim.keymap
+		keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+		-- jj as <Esc>
+		keymap.set("i", "jj", "<Esc>", { desc = "escape" })
+
+		-- Diagnostic keymaps
+		keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+		keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+		keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+		keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+		-- substitute keymaps
+		keymap.set("x", "<leader>rb", 'y:%s/<C-r><C-r>"//g<Left><Left>')
+		keymap.set("n", "<leader>rb", 'yiw:%s/<C-r><C-r>"//g<Left><Left>')
 		require("which-key").setup()
 
-		-- Document existing key chains
 		require("which-key").add({
-			-- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-			-- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-			-- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-			-- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-			-- ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-			--  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-			{ "<leader>*", "*``cgn", desc = "edit astarisk focus words" },
-			{
-				-- Nested mappings are allowed and can be added in any order
-				-- Most attributes can be inherited or overridden on any level
-				-- There's no limit to the depth of nesting
-				mode = { "n", "v" }, -- NORMAL and VISUAL mode
-				{ "<leader>Q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
-				{ "<leader>W", "<cmd>w<cr>", desc = "Write" },
-			},
-			-- },
+			{ "<leader>*", "*``cgn", desc = "edit asterisk focus words" },
 			{ "<leader>h", desc = "Git [H]unk", mode = { "n", "v" } },
 		})
 	end,
