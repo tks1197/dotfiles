c159321e95dd3a27a54d9a36047bdf477bc74f7f
tks1197 <t.koike929@gmail.com>
2025-01-01 17:56:37 +0900

    refactor(nvim): migrate which-key.nvim to mini.clue

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- keymaps definitions (built-in only)
+local keymap = vim.keymap
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- jj as <Esc>
+keymap.set("i", "jj", "<Esc>", { desc = "escape" })
+
+-- Diagnostic keymaps
+keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- substitute keymaps
+keymap.set("x", "<leader>rb", 'y:%s/<C-r><C-r>"//g<Left><Left>')
+keymap.set("n", "<leader>rb", 'yiw:%s/<C-r><C-r>"//g<Left><Left>')
+keymap.set("n", "<leader>*", "*``cgn", { desc = "edit asterisk focus words" })
--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,4 +1,5 @@
-return { -- Collection of various small independent plugins/modules
+-- https://github.com/echasnovski/mini.nvim
+return {
 	"echasnovski/mini.nvim",
 	config = function()
 		-- Better Around/Inside textobjects
@@ -16,7 +17,68 @@ return { -- Collection of various small independent plugins/modules
 		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
 
-		require("mini.files").setup()
+		local miniclue = require("mini.clue")
+		miniclue.setup({
+			window = {
+				delay = 200,
+				config = {
+					width = 50,
+					height = 15,
+				},
+			},
+			triggers = {
+				-- Leader triggers
+				{ mode = "n", keys = "<Leader>" },
+				{ mode = "x", keys = "<Leader>" },
+
+				-- Built-in completion
+				{ mode = "i", keys = "<C-x>" },
+
+				-- `g` key
+				{ mode = "n", keys = "g" },
+				{ mode = "x", keys = "g" },
+
+				-- Marks
+				{ mode = "n", keys = "'" },
+				{ mode = "n", keys = "`" },
+				{ mode = "x", keys = "'" },
+				{ mode = "x", keys = "`" },
+				-- Registers
+				{ mode = "n", keys = '"' },
+				{ mode = "x", keys = '"' },
+				{ mode = "i", keys = "<C-r>" },
+				{ mode = "c", keys = "<C-r>" },
+
+				-- Window commands
+				{ mode = "n", keys = "<C-w>" },
+
+				-- `z` key
+				{ mode = "n", keys = "z" },
+				{ mode = "x", keys = "z" },
+			},
+
+			clues = {
+				-- Enhance this by adding descriptions for <Leader> mapping groups
+				miniclue.gen_clues.builtin_completion(),
+				miniclue.gen_clues.g(),
+				miniclue.gen_clues.marks(),
+				miniclue.gen_clues.registers(),
+				miniclue.gen_clues.windows(),
+				miniclue.gen_clues.z(),
+			},
+		})
+
+		require("mini.files").setup({
+			mappings = {
+				go_in = "<CR>",
+				go_in_plus = "J",
+				go_out = "<BS>",
+				reset = "R",
+			},
+			windows = {
+				preview = true,
+			},
+		})
 		require("mini.diff").setup()
 
 		-- Simple and easy statusline.
