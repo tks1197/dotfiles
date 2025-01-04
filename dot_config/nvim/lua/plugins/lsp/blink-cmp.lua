b9255d5438341587eb6faa4af67775e58072f405
tks1197 <t.koike929@gmail.com>
2025-01-04 21:59:02 +0900

    chore(nvim): adjust lsp signature

--- dot_config/nvim/lua/plugins/lsp/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/lsp/blink-cmp.lua
@@ -24,7 +24,7 @@ return {
 		completion = {
 			documentation = {
 				-- Controls whether the documentation window will automatically show when selecting a completion item
-				auto_show = true,
+				auto_show = false,
 				-- Delay before showing the documentation window
 				auto_show_delay_ms = 250,
 				-- Delay before updating the documentation window when selecting a new item,
@@ -62,26 +62,8 @@ return {
 		},
 		signature = {
 			enabled = true,
-			trigger = {
-				blocked_trigger_characters = {},
-				blocked_retrigger_characters = {},
-				-- When true, will show the signature help window when the cursor comes after a trigger character when entering insert mode
-				show_on_insert_on_trigger_character = true,
-			},
 			window = {
-				min_width = 1,
-				max_width = 100,
-				max_height = 10,
 				border = "rounded",
-				winblend = 0,
-				winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
-				scrollbar = false, -- Note that the gutter will be disabled when border ~= 'none'
-				-- Which directions to show the window,
-				-- falling back to the next direction when there's not enough space,
-				-- or another window is in the way
-				direction_priority = { "n", "s" },
-				-- Disable if you run into performance issues
-				treesitter_highlighting = true,
 			},
 		},
 		appearance = {
