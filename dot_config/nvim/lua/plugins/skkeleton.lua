d7c3ad43c73c0bbff17671e01e8ba6ec9cc6b693
tks1197 <t.koike929@gmail.com>
2025-01-17 20:40:16 +0900

    chore(nvim): use skk global dictionary

--- dot_config/nvim/lua/plugins/skkeleton.lua
+++ dot_config/nvim/lua/plugins/skkeleton.lua
@@ -13,7 +13,15 @@ return {
 	config = function()
 		-- see https://github.com/vim-skk/skkeleton/blob/main/doc/skkeleton.jax
 		vim.fn["skkeleton#config"]({
-			sources = { "skk_server" },
+			globalDictionaries = {
+				"~/.local/share/skk/SKK-JISYO-USER.euc",
+				"~/.local/share/skk/SKK-JISYO.L",
+				"~/.local/share/skk/SKK-JISYO.station",
+				"~/.local/share/skk/SKK-JISYO.jinmei",
+				"~/.local/share/skk/SKK-JISYO.propernoun",
+				"~/.local/share/skk/SKK-JISYO.geo",
+			},
+			sources = { "skk_dictionary", "skk_server" },
 			userDictionary = "~/.local/share/skkeleton/SKK-JISYO-USER.utf8",
 		})
 	end,
