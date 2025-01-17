b2d9b6cb6d9df1f4be9d0181b65020848b0fc01d
tks1197 <t.koike929@gmail.com>
2025-01-17 20:40:57 +0900

    feat(nvim): add useful autocmds

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,12 +1,12 @@
 vim.loader.enable()
 
 require("config.options")
+require("config.autocmds")
 require("config.filetypes")
 require("config.lazy")
 vim.api.nvim_create_autocmd("User", {
 	pattern = "VeryLazy",
 	callback = function()
-		require("config.autocmds")
 		require("config.keymaps")
 	end,
 })
