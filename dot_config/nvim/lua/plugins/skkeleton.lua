b4a70b2600801b8b79efa65b2bf138a976cf0aa8
tks1197 <t.koike929@gmail.com>
2025-02-06 20:54:29 +0900

    chore(skk): update keybind

--- dot_config/nvim/lua/plugins/skkeleton.lua
+++ dot_config/nvim/lua/plugins/skkeleton.lua
@@ -22,7 +22,7 @@ return {
 			eggLikeNewline = true,
 		})
 		vim.fn["skkeleton#initialize"]()
-		vim.keymap.set({ "t", "i", "c" }, "<C-j>", "<Plug>(skkeleton-enable)")
+		vim.keymap.set({ "t", "i", "c" }, "<C-i>", "<Plug>(skkeleton-enable)")
 		vim.keymap.set({ "t", "i", "c" }, "<l>", "<Plug>(skkeleton-disable)")
 	end,
 }
