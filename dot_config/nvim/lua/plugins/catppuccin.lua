9f30efba77f443bebcb819c389857615e8f82bc4
tks1197 <t.koike929@gmail.com>
2025-02-07 00:26:58 +0900

    chore: update color scheme

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -4,10 +4,10 @@ return {
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
-			flavour = "auto", -- latte, frappe, macchiato, mocha
+			flavour = "frappe", -- latte, frappe, macchiato, mocha
 			background = { -- :h background
-				light = "latte",
-				dark = "macchiato",
+				light = "auto",
+				dark = "auto",
 			},
 			transparent_background = false, -- disables setting the background color.
 			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
