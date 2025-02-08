851ed6f70186e1d5f2e0d57e991a823d5e939335
tks1197 <t.koike929@gmail.com>
2025-02-08 23:52:46 +0900

    feat(textlint): add rules

--- scripts/macos/install_textlint.sh
+++ scripts/macos/install_textlint.sh
@@ -6,7 +6,9 @@ mkdir -p ~/.local/share/textlint
 
 pushd ~/.local/share/textlint || exit
 npm install --save-dev textlint
-jq -r '.rules | keys[]' ~/.config/textlint/.textlintrc.json | xargs --max-proc 4 -I {} npm install --save-dev textlint-rule-{}
+jq -r '.rules | keys[]' ~/.config/textlint/.textlintrc.json |
+  sed -e 's/\//\/textlint-rule-/' -e t -e 's/^/textlint-rule-/' -e t |
+  xargs -rn 1 npm install --save-dev
 popd || exit
 
 ln -sf ~/.local/share/textlint/node_modules/.bin/textlint ~/.local/bin/textlint
