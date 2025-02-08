73db9d35b6c385410b0017316cdd750b1383652c
tks1197 <t.koike929@gmail.com>
2025-02-08 18:34:11 +0900

    feat(nvim): add textlint

--- dot_config/nvim/lua/plugins/linter.lua
+++ dot_config/nvim/lua/plugins/linter.lua
@@ -1,12 +1,61 @@
+# TODO: refactor this file
 return {
 	"mfussenegger/nvim-lint",
 	event = { "BufEnter", "BufWritePost", "InsertLeave" },
 	config = function()
 		local lint = require("lint")
+		local severities = {
+			vim.diagnostic.severity.WARN,
+			vim.diagnostic.severity.ERROR,
+		}
+		lint.linters.textlint = {
+			name = "textlint",
+			cmd = "textlint",
+			stdin = true,
+			stream = "stdout",
+			args = {
+				"--format",
+				"json",
+				"--stdin",
+				"--stdin-filename",
+				function()
+					return vim.api.nvim_buf_get_name(0)
+				end,
+				"--config",
+				vim.fn.expand("$HOME/.config/textlint/.textlintrc.json"),
+			},
+			ignore_exitcode = true,
+			parser = function(output)
+				if output == "" then
+					return {}
+				end
+				local decoded = vim.json.decode(output)
+				if decoded == nil then
+					return {}
+				end
+				local diagnostics = {}
+				for _, value in ipairs(decoded) do
+					for _, item in ipairs(value.messages) do
+						table.insert(diagnostics, {
+							message = item.message,
+							col = item.loc.start.column - 1,
+							end_col = item.loc["end"].column - 1,
+							lnum = item.loc.start.line - 1,
+							end_lnum = item.loc["end"].line - 1,
+							code = item.ruleId,
+							severity = severities[item.severity],
+							source = "textlint",
+						})
+					end
+				end
+				return diagnostics
+			end,
+		}
 		lint.linters_by_ft = {
 			markdown = {
 				"markdownlint-cli2",
 				"cspell",
+				"textlint",
 			},
 			yaml = { "yamllint", "cspell" },
 			json = { "cspell" },
@@ -53,7 +102,7 @@ return {
 		}
 		local augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 		local _callback = function()
-			lint.try_lint(nil, { ignore_errors = true })
+			lint.try_lint(nil, { ignore_errors = false })
 		end
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = augroup,
