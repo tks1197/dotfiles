43b80e67a23ef4ee6d52502a5400d275d87c740b
tks1197 <t.koike929@gmail.com>
2025-02-08 16:05:03 +0900

    chore(bootstrap): adjust max-proc

--- scripts/macos/install_textlint.sh
+++ scripts/macos/install_textlint.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.local/share/textlint
 
 pushd ~/.local/share/textlint || exit
 npm install --save-dev textlint
-jq -r '.rules | keys[]' ~/.config/textlint/.textlintrc.json | xargs --max-proc 10 -I {} npm install --save-dev textlint-rule-{}
+jq -r '.rules | keys[]' ~/.config/textlint/.textlintrc.json | xargs --max-proc 4 -I {} npm install --save-dev textlint-rule-{}
 popd || exit
 
 ln -sf ~/.local/share/textlint/node_modules/.bin/textlint ~/.local/bin/textlint
