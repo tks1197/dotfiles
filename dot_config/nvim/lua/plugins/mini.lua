89a4c3f129beeef6d0a1ee4d2937405000650dd8
tks1197 <t.koike929@gmail.com>
2025-02-09 22:38:50 +0900

    chore(nvim): update mini.ai

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -8,7 +8,41 @@ return {
 		--  - va)  - [V]isually select [A]round [)]paren
 		--  - yinq - [Y]ank [I]nside [N]ext [']quote
 		--  - ci'  - [C]hange [I]nside [']quote
-		require("mini.ai").setup({ n_lines = 500 })
+		require("mini.ai").setup({
+			-- Table with textobject id as fields, textobject specification as values.
+			-- Also use this to disable builtin textobjects. See |MiniAi.config|.
+			custom_textobjects = nil,
+
+			-- Module mappings. Use `''` (empty string) to disable one.
+			mappings = {
+				-- Main textobject prefixes
+				around = "a",
+				inside = "i",
+
+				-- Next/last variants
+				around_next = "an",
+				inside_next = "in",
+				around_last = "al",
+				inside_last = "il",
+
+				-- Move cursor to corresponding edge of `a` textobject
+				goto_left = "g[",
+				goto_right = "g]",
+			},
+
+			-- Number of lines within which textobject is searched
+			n_lines = 50,
+
+			-- How to search for object (first inside current line, then inside
+			-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+			-- 'cover_or_nearest', 'next', 'previous', 'nearest'.
+			search_method = "cover_or_next",
+
+			-- Whether to disable showing non-error feedback
+			-- This also affects (purely informational) helper messages shown after
+			-- idle time if user input is required.
+			silent = false,
+		})
 
 		-- Add/delete/replace surroundings (brackets, quotes, etc.)
 		--
@@ -24,8 +58,8 @@ return {
 			window = {
 				delay = 200,
 				config = {
-					width = 50,
-					height = 15,
+					width = 45,
+					height = 25,
 				},
 			},
 			triggers = {
