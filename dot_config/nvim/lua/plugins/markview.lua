07610f1bcb2dbf6d894b879f9d6a840898ce8462
tks1197 <t.koike929@gmail.com>
2025-02-11 02:11:22 +0900

    chore(nvim): update markdown preview setting

--- dot_config/nvim/lua/plugins/markview.lua
+++ dot_config/nvim/lua/plugins/markview.lua
@@ -1,5 +1,16 @@
--- For `plugins/markview.lua` users.
+-- https://github.com/OXY2DEV/markview.nvim
 return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
+	config = function()
+		local presets = require("markview.presets")
+
+		require("markview").setup({
+			markdown = {
+				headings = presets.headings.glow,
+				horizontal_rules = presets.horizontal_rules.dashed,
+				table = presets.tables.rounded,
+			},
+		})
+	end,
 }
