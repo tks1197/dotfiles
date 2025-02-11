6ad92d8d6bd60430e84442f93701c83982fb61fa
tks1197 <t.koike929@gmail.com>
2025-02-11 14:36:25 +0900

    feat(nvim): add create new org file keymap

--- dot_config/nvim/lua/plugins/org-mode.lua
+++ dot_config/nvim/lua/plugins/org-mode.lua
@@ -26,6 +26,32 @@ return {
 					},
 				},
 			})
+			local function _create_org_file(input)
+				local org_dir = vim.fn.expand("~/Documents/org/") -- The org directory
+
+				-- Validate the input
+				if input == nil or input == "" then
+					print("Invalid filename.")
+					return
+				end
+
+				-- Ensure the filename has the .org extension
+				local filename = input:match(".*%.org$") and input or input .. ".org"
+				local filepath = org_dir .. filename
+
+				-- Create the file and open it in a new tab
+				vim.cmd("tabnew " .. filepath)
+				vim.notify("Created new org file in a new tab: " .. filepath)
+			end
+			local function create_org_file()
+				vim.ui.input({ prompt = "Create new org file" }, _create_org_file)
+			end
+			vim.keymap.set(
+				"n",
+				"<leader>oc",
+				create_org_file,
+				{ noremap = true, silent = true, desc = "Create new org file" }
+			)
 		end,
 	},
 	{
