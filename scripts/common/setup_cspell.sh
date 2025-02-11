d44ac65814a717d7b41e87decac911b9d2062bef
tks1197 <t.koike929@gmail.com>
2025-02-11 18:38:29 +0900

    chore(bootstrap): add cspell dict install script

--- scripts/common/setup_cspell.sh
+++ scripts/common/setup_cspell.sh
@@ -6,6 +6,8 @@ mkdir -p ~/.local/share/cspell
 
 pushd ~/.local/share/cspell || exit
 npm install --save-dev git+https://github.com/streetsidesoftware/cspell-cli
+jq -r '.import[] | split("/")[:2] | join("/")' ~/.config/cspell/cspell.json |
+  xargs -n 1 npm install --save-dev
 popd || exit
 
 ln -sf ~/.local/share/cspell/node_modules/.bin/cspell ~/.local/bin/cspell
