86e7d9ac9f3f4bdf46e78f48750f0010f89dc0cb
tks1197 <t.koike929@gmail.com>
2025-02-12 20:32:54 +0900

    refactor(bootstrap): cspell

--- scripts/common/setup_cspell.sh
+++ scripts/common/setup_cspell.sh
@@ -6,7 +6,8 @@ mkdir -p ~/.local/share/cspell
 
 pushd ~/.local/share/cspell || exit
 npm install --save-dev git+https://github.com/streetsidesoftware/cspell-cli
-jq -r '.import[] | split("/")[:2] | join("/")' ~/.config/cspell/cspell.json |
+jq -r '.import[]' ~/.config/cspell/cspell.json |
+  cut -d '/' -f 1-2 |
   xargs -n 1 npm install --save-dev
 popd || exit
 
