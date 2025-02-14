7e44bfbe377defc08fbf95db478e2e3c840d971c
tks1197 <t.koike929@gmail.com>
2025-02-14 22:50:56 +0900

    feat(colorscheme): add rose-pine

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -11,8 +11,16 @@ return {
 	},
 	{
 		"EdenEast/nightfox.nvim",
+		enabled = false,
 		config = function()
 			vim.cmd("colorscheme dawnfox")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine-dawn")
 		end,
 	},
 	{
