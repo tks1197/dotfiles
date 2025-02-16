11221d9015a380ebbfa1d3bb3b174c30fb6804af
tks1197 <t.koike929@gmail.com>
2025-02-16 21:01:44 +0900

    feat(nvim): add fzf-org

--- dot_config/nvim/lua/plugins/org-mode.lua
+++ dot_config/nvim/lua/plugins/org-mode.lua
@@ -71,4 +71,39 @@ return {
     config = true,
     ft = 'org',
   },
+  {
+    '0xzhzh/fzf-org.nvim',
+    lazy = false, -- lazy loading is handled internally
+    dependencies = {
+      'ibhagwan/fzf-lua',
+      'nvim-orgmode/orgmode',
+    },
+    keys = {
+      -- example keybindings
+      {
+        '<leader>og',
+        function()
+          require('fzf-org').orgmode()
+        end,
+        desc = 'org-browse',
+      },
+      {
+        '<leader>of',
+        function()
+          require('fzf-org').files()
+        end,
+        desc = 'org-files',
+      },
+      {
+        '<leader>or',
+        function()
+          require('fzf-org').refile_to_file()
+        end,
+        desc = 'org-refile',
+      },
+    },
+    config = function()
+      require('fzf-org').setup {} -- see below
+    end,
+  },
 }
