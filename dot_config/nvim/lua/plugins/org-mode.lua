ef679761d91927cfa8dbb33f75790c4379b303ef
tks1197 <t.koike929@gmail.com>
2025-02-17 21:04:37 +0900

    chore(org-mode): change file location

--- dot_config/nvim/lua/plugins/org-mode.lua
+++ dot_config/nvim/lua/plugins/org-mode.lua
@@ -9,7 +9,7 @@ return {
       require('orgmode').setup({
         org_agenda_files = '~/Documents/org/**/*',
         org_default_notes_file = '~/Documents/org/refile.org',
-        win_split_mode = 'horizontal',
+        win_split_mode = 'auto',
         win_border = 'rounded',
         org_startup_folded = 'overview',
         org_agenda_span = 'day',
@@ -23,7 +23,7 @@ return {
           t = {
             description = 'TODO',
             template = '** TODO %?\n',
-            target = vim.fn.expand('~/Documents/org/agenda/todo.org'),
+            target = vim.fn.expand('~/Documents/org/todo.org'),
             headline = 'Inbox',
           },
           n = { description = 'Note', template = '- %<%H:%M:%S> %?' },
@@ -39,32 +39,32 @@ return {
       vim.keymap.set('n', 'gT', function()
         require('orgmode').capture:open_template_by_shortcut('t')
       end, { noremap = true, silent = true, desc = 'Open Org Capture(TODO)' })
-      local function _create_org_file(input)
-        local org_dir = vim.fn.expand('~/Documents/org/') -- The org directory
-
-        -- Validate the input
-        if input == nil or input == '' then
-          print('Invalid filename.')
-          return
-        end
-
-        -- Ensure the filename has the .org extension
-        local filename = input:match('.*%.org$') and input or input .. '.org'
-        local filepath = org_dir .. filename
-
-        -- Create the file and open it in a new tab
-        vim.cmd('tabnew ' .. filepath)
-        vim.notify('Created new org file in a new tab: ' .. filepath)
-      end
-      local function create_org_file()
-        vim.ui.input({ prompt = 'Create new org file' }, _create_org_file)
-      end
-      vim.keymap.set(
-        'n',
-        '<leader>oc',
-        create_org_file,
-        { noremap = true, silent = true, desc = 'Create new org file' }
-      )
+      -- local function _create_org_file(input)
+      --   local org_dir = vim.fn.expand('~/Documents/org/') -- The org directory
+      --
+      --   -- Validate the input
+      --   if input == nil or input == '' then
+      --     print('Invalid filename.')
+      --     return
+      --   end
+      --
+      --   -- Ensure the filename has the .org extension
+      --   local filename = input:match('.*%.org$') and input or input .. '.org'
+      --   local filepath = org_dir .. filename
+      --
+      --   -- Create the file and open it in a new tab
+      --   vim.cmd('tabnew ' .. filepath)
+      --   vim.notify('Created new org file in a new tab: ' .. filepath)
+      -- end
+      -- local function create_org_file()
+      --   vim.ui.input({ prompt = 'Create new org file' }, _create_org_file)
+      -- end
+      -- vim.keymap.set(
+      --   'n',
+      --   '<leader>oc',
+      --   create_org_file,
+      --   { noremap = true, silent = true, desc = 'Create new org file' }
+      -- )
     end,
   },
   {
