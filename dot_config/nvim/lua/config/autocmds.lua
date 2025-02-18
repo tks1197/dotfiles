c19ffa28375d15ab269484e42f6a1157d3392741
tks1197 <t.koike929@gmail.com>
2025-02-18 21:57:49 +0900

    chore(nvim): add iwe setting

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -75,3 +75,17 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
   end,
 })
+
+-- vim.api.nvim_create_autocmd('FileType', {
+--   pattern = 'markdown',
+--   callback = function(args)
+--     vim.lsp.start({
+--       name = 'iwes',
+--       cmd = { 'iwes' },
+--       root_dir = vim.fs.root(args.buf, { '.iwe' }),
+--       flags = {
+--         debounce_text_changes = 500,
+--       },
+--     })
+--   end,
+-- })
