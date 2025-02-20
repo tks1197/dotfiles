5119a45f9eb2679329b21e6e12511625a76dd503
tks1197 <t.koike929@gmail.com>
2025-02-20 20:32:21 +0900

    feat(skk, linux): auto switch input source

---
+++ scripts/linux/switch_ime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+swaymsg -m -t subscribe '["window"]' |
+  stdbuf -o L jq -c 'select(.change == "focus")' |
+  while read -r string; do
+    app_id=$(echo "$string" | jq -r '.container.app_id')
+    if [[ "$app_id" == "zen-beta" ]]; then
+      fcitx5-remote -s skk
+    fi
+  done
