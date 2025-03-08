5e077907455b578c420c63a1e84ae5c94fe2b697
tks1197 <t.koike929@gmail.com>
2025-03-08 18:51:09 +0900

    chore: add skk server setup

--- scripts/skk/setup_skk.sh
+++ scripts/skk/setup_skk.sh
@@ -8,4 +8,12 @@ mkdir -p ~/.config/skk
 mkdir -p ~/.local/share/skk/
 
 # TODO: download skk-jisyo
-# yaskkerv2 setup 
+if ! command -v yaskkserv2 2>&1 >/dev/null; then
+  echo "yaskkserv2 not found installing..."
+  gh get wachikun/yaskkserv2
+  pushd ~/src/github.com/wachikun/yaskkserv2 || exit
+  cargo build --release
+  cp -av target/release/yaskkserv2 /usr/local/sbin/yaskkserv2
+  cp -av target/release/yaskkserv2_make_dictionary ~/.local/bin/yaskkserv2_make_dictionary
+  popd || exit
+fi
