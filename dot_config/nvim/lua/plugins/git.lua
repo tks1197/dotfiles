21932de02acfba8503b68c292f7d51adf5bb37f1
tks1197 <t.koike929@gmail.com>
2025-03-13 21:09:17 +0900

    chore(nvim): update diffview.nvim keymap

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -3,22 +3,49 @@ return {
     'sindrets/diffview.nvim',
     cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory' },
     event = 'VeryLazy',
+    init = function()
+      vim.opt.diffopt = {
+        'internal',
+        'filler',
+        'closeoff',
+        'context:12',
+        'algorithm:histogram',
+        'linematch:200',
+        'indent-heuristic',
+      }
+    end,
     config = function()
-      vim.keymap.set('n', '<leader>hh', '<cmd>DiffviewFileHistory<cr>', { desc = 'Repo history' })
-      vim.keymap.set('n', '<leader>hf', '<cmd>DiffviewFileHistory --follow .<cr>', { desc = 'File history' })
-      vim.keymap.set('n', '<leader>hl', '<Cmd>.DiffviewFileHistory --follow<CR>', { desc = 'Line history' })
+      local function toggle_diffview(cmd)
+        if next(require('diffview.lib').views) == nil then
+          vim.cmd(cmd)
+        else
+          vim.cmd('DiffviewClose')
+        end
+      end
+      vim.keymap.set('n', '<leader>hh', function()
+        toggle_diffview('DiffviewFileHistory')
+      end, { desc = 'Repo history' })
+      vim.keymap.set('n', '<leader>hf', function()
+        toggle_diffview('DiffviewFileHistory --follow .')
+      end, { desc = 'File history' })
+      vim.keymap.set('n', '<leader>hl', function()
+        toggle_diffview('DiffviewFileHistory --follow')
+      end, { desc = 'Line history' })
+      vim.keymap.set('n', '<leader>hd', function()
+        toggle_diffview('DiffviewOpen')
+      end, { desc = 'Repo diff' })
       vim.keymap.set('n', '<leader>hc', '<Cmd>DiffviewClose<CR>', { desc = 'Close Diffview' })
-      vim.keymap.set('n', '<leader>hd', '<cmd>DiffviewOpen<cr>', { desc = 'Repo diff' })
+
       local function get_default_branch_name()
         local res = vim.system({ 'git', 'rev-parse', '--verify', 'main' }, { capture_output = true }):wait()
         return res.code == 0 and 'main' or 'master'
       end
       vim.keymap.set('n', '<leader>hm', function()
-        vim.cmd('DiffviewOpen ' .. get_default_branch_name())
+        toggle_diffview('DiffviewOpen ' .. get_default_branch_name())
       end, { desc = 'Diff against master' })
 
       vim.keymap.set('n', '<leader>hM', function()
-        vim.cmd('DiffviewOpen HEAD..origin/' .. get_default_branch_name())
+        toggle_diffview('DiffviewOpen HEAD..origin/' .. get_default_branch_name())
       end, { desc = 'Diff against origin/master' })
     end,
   },
