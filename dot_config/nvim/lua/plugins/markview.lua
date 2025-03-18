3dd1b5def3c2f68430955a3adf8805c765a0f20a
tks1197 <t.koike929@gmail.com>
2025-03-18 19:35:43 +0900

    chore(nvim): update markview https://github.com/OXY2DEV/markview.nvim/wiki/Advanced-usage#-disabling-indentation-of-top-level-list-items

--- dot_config/nvim/lua/plugins/markview.lua
+++ dot_config/nvim/lua/plugins/markview.lua
@@ -13,7 +13,22 @@ return {
         horizontal_rules = presets.horizontal_rules.dashed,
         table = presets.tables.rounded,
         list_items = {
-          shift_width = 2,
+          shift_width = function(buffer, item)
+            --- Reduces the `indent` by 1 level.
+            ---
+            ---         indent                      1
+            --- ------------------------- = 1 ÷ --------- = new_indent
+            --- indent * (1 / new_indent)       new_indent
+            ---
+            local parent_indnet = math.max(1, item.indent - vim.bo[buffer].shiftwidth)
+
+            return item.indent * (1 / (parent_indnet * 2))
+          end,
+          marker_minus = {
+            add_padding = function(_, item)
+              return item.indent > 1
+            end,
+          },
         },
       },
     })
