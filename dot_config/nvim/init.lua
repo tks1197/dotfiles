9a3d1fc16e024eff172cad619c6000f5f9f63c4f
tks1197 <t.koike929@gmail.com>
2025-04-11 23:51:35 +0900

    chore(nvim): vscode neovim setting

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,12 +1,14 @@
 vim.loader.enable()
 
-require('config.options')
-require('config.autocmds')
-require('config.filetypes')
-require('config.lazy')
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'VeryLazy',
-  callback = function()
-    require('config.keymaps')
-  end,
-})
+if not vim.g.vscode then
+  require('config.options')
+  require('config.autocmds')
+  require('config.filetypes')
+  require('config.lazy')
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'VeryLazy',
+    callback = function()
+      require('config.keymaps')
+    end,
+  })
+end
