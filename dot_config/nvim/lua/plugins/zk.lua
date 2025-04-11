07b7861f5267dfaeeb3dad3edc42c42a382f7743
tks1197 <t.koike929@gmail.com>
2025-04-12 01:39:08 +0900

    feat(nvim,zk): add ZkDaily Command

--- dot_config/nvim/lua/plugins/zk.lua
+++ dot_config/nvim/lua/plugins/zk.lua
@@ -1,8 +1,17 @@
 return {
   'zk-org/zk-nvim',
   config = function()
-    require('zk').setup({
-      -- See Setup section below
-    })
+    local zk = require('zk')
+    local commands = require('zk.commands')
+    commands.add('ZkDaily', function(options)
+      options = vim.tbl_extend(
+        'force',
+        { dir = 'journal/daily', group = 'daily', template = 'daily.md', edit = true },
+        options or {}
+      )
+      zk.new(options)
+    end)
+    vim.keymap.set({ 'n' }, '<leader>zd', '<Cmd>ZkDaily<CR>', { desc = 'Open Daily Note' })
+    zk.setup()
   end,
 }
