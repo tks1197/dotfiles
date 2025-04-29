b0bfc251287506e55b050024656cf822c6bb17ce
tks1197 <t.koike929@gmail.com>
2025-04-29 22:54:55 +0900

    chore(nvim): use typos-lsp instead of cspell

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -99,21 +99,21 @@ return {
         },
       },
       -- https://github.com/tekumara/typos-lsp
-      -- typos_lsp = {
-      -- 	{
-      -- 		-- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
-      -- 		cmd_env = { RUST_LOG = "error" },
-      -- 		init_options = {
-      -- 			-- Custom config. Used together with a config file found in the workspace or its parents,
-      -- 			-- taking precedence for settings declared in both.
-      -- 			-- Equivalent to the typos `--config` cli argument.
-      -- 			config = "~/.config/typos_lsp/typos.toml",
-      -- 			-- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
-      -- 			-- Defaults to error.
-      -- 			diagnosticSeverity = "Error",
-      -- 		},
-      -- 	},
-      -- },
+      typos_lsp = {
+        {
+          -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+          cmd_env = { RUST_LOG = 'error' },
+          init_options = {
+            -- Custom config. Used together with a config file found in the workspace or its parents,
+            -- taking precedence for settings declared in both.
+            -- Equivalent to the typos `--config` cli argument.
+            config = '~/.config/typos_lsp/typos.toml',
+            -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
+            -- Defaults to error.
+            diagnosticSeverity = 'Error',
+          },
+        },
+      },
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
