c6d9a26c0cf113ce0e0bd5edfed1258d6b246c16
tks1197 <t.koike929@gmail.com>
2025-05-01 00:56:41 +0900

    chore(nvim): update markview.nvim

--- dot_config/nvim/lua/plugins/markview.lua
+++ dot_config/nvim/lua/plugins/markview.lua
@@ -11,9 +11,22 @@ return {
     require('markview').setup({
       preview = {
         filetypes = {
+          'md',
           'markdown',
           'codecompanion',
         },
+        ignore_buftypes = {},
+        condition = function(buffer)
+          local ft, bt = vim.bo[buffer].ft, vim.bo[buffer].bt
+
+          if bt == 'nofile' and ft == 'codecompanion' then
+            return true
+          elseif bt == 'nofile' then
+            return false
+          else
+            return true
+          end
+        end,
       },
       code_blocks = {
         style = 'simple',
