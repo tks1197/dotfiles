a0b1f9806b92dbcbe3b868fde5ecd99e60e00205
tks1197 <t.koike929@gmail.com>
2025-05-05 18:28:47 +0900

    fix(nvim): revert blink.cmp setting for markview.nvim

--- dot_config/nvim/lua/plugins/markview.lua
+++ dot_config/nvim/lua/plugins/markview.lua
@@ -2,6 +2,9 @@
 return {
   'OXY2DEV/markview.nvim',
   lazy = false,
+  dependencies = {
+    'saghen/blink.cmp',
+  },
   enabled = true,
   config = function()
     local presets = require 'markview.presets'
