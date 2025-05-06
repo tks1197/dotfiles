426a2c331e6b62599eaf7d652fbffcb4848913e7
tks1197 <t.koike929@gmail.com>
2025-05-07 08:54:56 +0900

    chore(chezmoi): change sourceDir

--- scripts/skk/start_yaskkserv2.sh
+++ scripts/skk/start_yaskkserv2.sh
@@ -5,4 +5,4 @@ pushd "$HOME/.local/share/skk/" || exit 1
   yaskkserv2 /tmp/dictionary.yaskkserv2 --google-suggest --google-cache-filename=/tmp/yaskkserv2-google-suggest.cache
 popd || exit 1
 
-<"$HOME/.local/share/chezmoi/scripts/skk/job.txt" crontab -
+<"$HOME/.dotfiles/scripts/skk/job.txt" crontab -
