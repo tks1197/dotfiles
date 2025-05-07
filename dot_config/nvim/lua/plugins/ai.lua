5c59e7fa8390315e4176c786de6897601b5d4467
tks1197 <t.koike929@gmail.com>
2025-05-07 23:10:37 +0900

    feat(nvim): hijack netrw

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -21,10 +21,44 @@ return {
       'MunifTanjim/nui.nvim',
       -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    init = function()
+      -- the remote file handling part
+      vim.api.nvim_create_autocmd('BufEnter', {
+        group = vim.api.nvim_create_augroup('RemoteFileInit', { clear = true }),
+        callback = function()
+          local f = vim.fn.expand('%:p')
+          for _, v in ipairs { 'dav', 'fetch', 'ftp', 'http', 'rcp', 'rsync', 'scp', 'sftp' } do
+            local p = v .. '://'
+            if f:sub(1, #p) == p then
+              vim.cmd [[
+              unlet g:loaded_netrw
+              unlet g:loaded_netrwPlugin
+              runtime! plugin/netrwPlugin.vim
+              silent Explore %
+            ]]
+              break
+            end
+          end
+          vim.api.nvim_clear_autocmds { group = 'RemoteFileInit' }
+        end,
+      })
+      vim.api.nvim_create_autocmd('BufEnter', {
+        group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
+        callback = function()
+          local f = vim.fn.expand('%:p')
+          if vim.fn.isdirectory(f) ~= 0 then
+            vim.cmd('Neotree current dir=' .. f)
+            vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
+          end
+        end,
+      })
+      -- keymaps
+    end,
     config = function()
       vim.keymap.set('n', 'gO', '<cmd>Neotree toggle<CR>', { desc = 'Open mini.files explorer' })
       require('neo-tree').setup({
         filesystem = {
+          hijack_netrw_behavior = 'open_current',
           commands = {
             avante_add_files = function(state)
               local node = state.tree:get_node()
@@ -52,6 +86,9 @@ return {
             enabled = true, -- This will find and focus the file in the active buffer every time
             --               -- the current file is changed while the tree is open.
             leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          },
+          filtered_items = {
+            visible = true,
           },
           window = {
             mappings = {
