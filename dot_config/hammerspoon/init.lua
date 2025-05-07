aec0b697abba2dc596711c28583446c77b52a346
tks1197 <t.koike929@gmail.com>
2025-05-07 09:07:11 +0900

    style(nvim): format init.lua

--- dot_config/hammerspoon/init.lua
+++ dot_config/hammerspoon/init.lua
@@ -1,5 +1,5 @@
 hs.window.filter.new():subscribe(hs.window.filter.windowFocused, function(window)
-  if window:application():name() == 'Zen Browser' then
-    hs.execute('/opt/homebrew/bin/macism jp.sourceforge.inputmethod.aquaskk')
+  if window:application():name() == "Zen Browser" then
+    hs.execute("/opt/homebrew/bin/macism jp.sourceforge.inputmethod.aquaskk")
   end
 end)
