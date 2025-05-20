10223cd2c20ceb217f607725427b6a4c5fa7857a
tks1197 <t.koike929@gmail.com>
2025-05-20 09:34:27 +0900

    chore(nvim): update keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,17 +6,19 @@ keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 keymap.set('i', 'jj', '<Esc>', { desc = 'escape' })
 
 -- Diagnostic keymaps
-keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- substitute keymaps
-keymap.set('x', '<leader>rb', 'y:%s/<C-r><C-r>"//g<Left><Left>')
-keymap.set('n', '<leader>rb', 'yiw:%s/<C-r><C-r>"//g<Left><Left>')
-keymap.set('n', '<leader>*', '*``cgn', { desc = 'edit asterisk focus words' })
+vim.keymap.set('n', '<leader>rb', ':%s/\\V\\<<C-r><C-w>\\>//g<Left><Left>', { noremap = true, silent = false })
+vim.keymap.set('x', '<leader>rv', '"zy:%s/\\V<C-r><C-r>=escape(@z,"/\\\\")<CR>//gce<Left><Left><Left><Left>', { noremap = true, silent = false })
+keymap.set('n', '<leader>r*', '*``cgn', { desc = 'edit asterisk focus words' })
 
 keymap.set('n', 'ycc', 'yygccp', { remap = true })
 keymap.set('n', 'J', 'mzJ`z:delmarks z<cr>')
 
 keymap.set('n', 'Y', 'y$', { noremap = true, silent = true })
+
+vim.keymap.set('n', 'p', ']p`]', { noremap = true, silent = true })
+vim.keymap.set('n', 'P', ']P`]', { noremap = true, silent = true })
+vim.keymap.set('x', 'p', 'P', { noremap = true, silent = true })
