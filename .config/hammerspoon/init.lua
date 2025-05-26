4055af9af1200a191bf49c7639f2ccc1108c97c3
tks1197 <t.koike929@gmail.com>
2025-05-26 20:31:53 +0900

    chore(macos): udpate hammerspoon

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,5 +1,5 @@
 hs.window.filter.new():subscribe(hs.window.filter.windowFocused, function(window)
-  if window:application():name() == "Firefox" then
-    hs.execute("/opt/homebrew/bin/macism jp.sourceforge.inputmethod.aquaskk")
+  if window:application():name() == 'Firefox' then
+    hs.execute '/opt/homebrew/bin/macism dev.ensan.inputmethod.azooKeyMac.Japanese'
   end
 end)
