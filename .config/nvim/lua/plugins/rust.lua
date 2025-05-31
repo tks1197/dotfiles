3d316a71871701763a747ed05bfba525f4337568
tks1197 <t.koike929@gmail.com>
2025-05-31 13:41:35 +0900

    feat(nvim): add rustowl

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -1,4 +1,10 @@
 return {
+  {
+    'cordx56/rustowl',
+    version = '*', -- Latest stable version
+    build = 'cargo binstall rustowl',
+    lazy = false, -- This plugin is already lazy
+  },
   -- https://github.com/mrcjkb/rustaceanvim
   {
     'mrcjkb/rustaceanvim',
