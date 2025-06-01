c58a6e738a6fae43324c318704786304dc1ac5de
tks1197 <t.koike929@gmail.com>
2025-06-01 22:15:48 +0900

    chore(nvim): update rust plugins

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -4,6 +4,15 @@ return {
     version = '*', -- Latest stable version
     build = 'cargo binstall rustowl',
     lazy = false, -- This plugin is already lazy
+    config = function()
+      require('rustowl').setup {
+        auto_attach = false, -- Auto attach the RustOwl LSP client when opening a Rust file
+        auto_enable = false, -- Enable RustOwl immediately when attaching the LSP client
+        idle_time = 500, -- Time in milliseconds to hover with the cursor before triggering RustOwl
+        client = {}, -- LSP client configuration that gets passed to `vim.lsp.start`
+        highlight_style = 'undercurl', -- You can also use 'underline'
+      }
+    end,
   },
   -- https://github.com/mrcjkb/rustaceanvim
   {
@@ -14,6 +23,34 @@ return {
       return not vim.g.vscode
     end,
     lazy = false, -- This plugin is already lazy
+    config = function()
+      vim.g.rustaceanvim = {
+        -- Plugin configuration
+        tools = {},
+        -- LSP configuration
+        server = {
+          on_attach = function(client, bufnr)
+            -- you can also put keymaps in here
+          end,
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ['rust-analyzer'] = {
+              checkOnSave = false,
+            },
+          },
+        },
+        -- DAP configuration
+        dap = {},
+      }
+    end,
+    -- opts = {
+    --   tools = {
+    --     enable_clippy = false,
+    --   },
+    -- },
+    -- config = function(_, opts)
+    --   require('rustaceanvim').setup(opts)
+    -- end,
   },
   {
     -- crates.nvim(A neovim plugin that helps managing crates.io dependencies)
