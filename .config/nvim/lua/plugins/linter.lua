89d8f5daff02228f6e11613b5f99595d713745c1
tks1197 <t.koike929@gmail.com>
2025-06-14 21:12:18 +0900

    chore(nvim): remove unused linter configuration

--- .config/nvim/lua/plugins/linter.lua
+++ .config/nvim/lua/plugins/linter.lua
@@ -6,47 +6,6 @@ return {
   end,
   config = function()
     local lint = require 'lint'
-
-    local severities = {
-      High = vim.diagnostic.severity.ERROR,
-      Medium = vim.diagnostic.severity.WARN,
-      Informational = vim.diagnostic.severity.INFO,
-    }
-
-    lint.linters.zizmor = {
-      name = 'zizmor',
-      cmd = 'zizmor',
-      args = { '--format', 'json' },
-      stdin = false,
-      ignore_exitcode = true, -- 14
-
-      parser = function(output, _)
-        local items = {}
-
-        if output == '' then
-          return items
-        end
-
-        local decoded = vim.json.decode(output) or {}
-        local bufpath = vim.fn.expand '%:p'
-
-        for _, diag in ipairs(decoded) do
-          if diag.locations[1].symbolic.key.Local.given_path == bufpath then
-            table.insert(items, {
-              source = 'zizmor',
-              lnum = diag.locations[1].concrete.location.start_point.row,
-              col = diag.locations[1].concrete.location.start_point.column,
-              end_lnum = diag.locations[1].concrete.location.end_point.row - 1,
-              end_col = diag.locations[1].concrete.location.end_point.column,
-              message = diag.desc,
-              severity = assert(severities[diag.determinations.severity], 'missing mapping for severity ' .. diag.determinations.severity),
-            })
-          end
-        end
-
-        return items
-      end,
-    }
     lint.linters_by_ft = {
       markdown = {
         'markdownlint-cli2',
