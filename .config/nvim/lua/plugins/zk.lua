19a4202d1b2bc17a46fd1fbdd4a5f8c1c23459d2
tks1197 <t.koike929@gmail.com>
2025-06-14 21:12:48 +0900

    chore(nvim): remove deprecated options

--- .config/nvim/lua/plugins/zk.lua
+++ .config/nvim/lua/plugins/zk.lua
@@ -256,12 +256,11 @@ return {
           -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
         end,
 
-        -- Optional, set to true if you use the Obsidian Advanced URI plugin.
-        -- https://github.com/Vinzent03/obsidian-advanced-uri
-        use_advanced_uri = false,
-
-        -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
-        open_app_foreground = false,
+        -- open = {
+        --   func = function(uri)
+        --     vim.ui.open(uri, { cmd = { 'open', '-a', '/Applications/Obsidian.app' } })
+        --   end,
+        -- },
 
         picker = {
           -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
