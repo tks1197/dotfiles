a13ac521b36443437f78739e75c4e60add80b168
tks1197 <t.koike929@gmail.com>
2025-06-14 23:29:09 +0900

    fix(nvim/linter): do not trigger linting on BufEnter

--- .config/nvim/lua/plugins/linter.lua
+++ .config/nvim/lua/plugins/linter.lua
@@ -1,6 +1,7 @@
 return {
   'mfussenegger/nvim-lint',
-  event = { 'BufEnter', 'BufWritePost' },
+  lazy = true,
+  event = { 'BufWritePost' },
   cond = function()
     return not vim.g.vscode
   end,
@@ -15,14 +16,14 @@ return {
       ghactions = { 'yamllint', 'actionlint', 'zizmor' },
       python = { 'ruff', 'flake8' },
     }
-    local augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-    local _callback = function()
-      lint.try_lint(nil, { ignore_errors = true })
-    end
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-      group = augroup,
-      callback = _callback,
+    vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+      group = vim.api.nvim_create_augroup('lint', { clear = true }),
+      callback = function()
+        lint.try_lint(nil, { ignore_errors = true })
+      end,
     })
-    vim.keymap.set('n', '<leader>l', _callback, { desc = 'Trigger linting for current file' })
+    vim.keymap.set('n', '<leader>l', function()
+      lint.try_lint(nil, { ignore_errors = true })
+    end, { desc = 'Trigger linting for current file' })
   end,
 }
