c2cfb1f7778586b80ecc8fdb039fae08afaeec80
tks1197 <t.koike929@gmail.com>
2025-06-17 23:01:17 +0900

    fix(nvim): only send bindkey command on first terminal creation
    Prevent bindkey command from being sent every time the terminal is toggled.
    Now uses a buffer-local flag to track if the command has already been sent.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -14,13 +14,14 @@ return {
             position = 'float',
             border = 'rounded',
             on_win = function(self)
-              -- Send bindkey command after terminal is ready
+              -- Send bindkey command only when terminal is first created
               vim.schedule(function()
                 if self.buf and vim.api.nvim_buf_is_valid(self.buf) then
                   local job_id = vim.b[self.buf].terminal_job_id
-                  if job_id then
+                  if job_id and not vim.b[self.buf].bindkey_sent then
                     -- Send the bindkey command for zsh autosuggestion
                     vim.fn.chansend(job_id, "bindkey '^F' autosuggest-accept\n")
+                    vim.b[self.buf].bindkey_sent = true
                   end
                 end
               end)
