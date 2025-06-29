f32c77e1c068d091171a5c39dc64614ca73c38dc
tks1197 <t.koike929@gmail.com>
2025-06-29 16:56:55 +0900

    feat(nvim): add user commands for copying file paths

--- .config/nvim/lua/config/user_commands.lua
+++ .config/nvim/lua/config/user_commands.lua
@@ -16,3 +16,37 @@ vim.api.nvim_create_user_command('OrgSync', function()
     stderr_buffered = true,
   })
 end, { desc = 'Sync orgfiles to dropbox using rclone' })
+
+local function get_range_str(opts)
+  if opts.range ~= 2 then
+    return ''
+  end
+  if opts.line1 == opts.line2 then
+    return '#L' .. opts.line1
+  end
+  return '#L' .. opts.line1 .. '-L' .. opts.line2
+end
+local function copy_path(opts, target)
+  local expr = '%'
+  if target == 'full path' then
+    expr = '%:p'
+  elseif target == 'file name' then
+    expr = '%:t'
+  end
+
+  local path = vim.fn.expand(expr) .. get_range_str(opts)
+  vim.fn.setreg('*', path)
+  vim.notify('Copied ' .. target .. ': ' .. path)
+end
+
+vim.api.nvim_create_user_command('CopyFullPath', function(opts)
+  copy_path(opts, 'full path')
+end, { range = true, desc = 'Copy the full path of the current file to the clipboard' })
+
+vim.api.nvim_create_user_command('CopyRelativePath', function(opts)
+  copy_path(opts, 'relative path')
+end, { range = true, desc = 'Copy the relative path of the current file to the clipboard' })
+
+vim.api.nvim_create_user_command('CopyFileName', function(opts)
+  copy_path(opts, 'file name')
+end, { range = true, desc = 'Copy the file name of the current file to the clipboard' })
