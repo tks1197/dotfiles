15e08efd14d3dbbd94a602f312c4c35909fcffa5
tks1197 <t.koike929@gmail.com>
2025-06-29 16:58:20 +0900

    chore(nvim): update git plugin configuration

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,21 +1,21 @@
 return {
-	{
-		"tpope/vim-fugitive",
-		-- lazy = true,
-		event = "CmdlineEnter",
-		cond = function()
-			return not vim.g.vscode
-		end,
-		config = function()
-			-- :Git -c pager.diff=delta diff
-			vim.api.nvim_create_user_command("DeltaDiff", function(args)
-				local vimCmd = "Git -c pager.diff=delta diff"
-				if args["args"] then
-					vimCmd = vimCmd .. " " .. args["args"]
-				end
-				vim.cmd(vimCmd)
-			end, { desc = "Git Diff for Delta", nargs = "*" })
-		end,
-	},
-	-- { 'tpope/vim-rhubarb', event = 'VeryLazy' },
+  {
+    'tpope/vim-fugitive',
+    -- lazy = true,
+    event = 'CmdlineEnter',
+    cond = function()
+      return not vim.g.vscode
+    end,
+    config = function()
+      -- :Git -c pager.diff=delta diff
+      vim.api.nvim_create_user_command('DeltaDiff', function(args)
+        local vimCmd = 'Git -c pager.diff=delta diff'
+        if args['args'] then
+          vimCmd = vimCmd .. ' ' .. args['args']
+        end
+        vim.cmd(vimCmd)
+      end, { desc = 'Git Diff for Delta', nargs = '*' })
+    end,
+  },
+  -- { 'tpope/vim-rhubarb', event = 'VeryLazy' },
 }
